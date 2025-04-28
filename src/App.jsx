1331c1549085e2b464dac9d77417b5ee850c596f
Keerthikotra <prasannakotra66@gmail.com>
2025-04-28 12:30:59 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import { Routes } from 'react-router-dom'
+import Home from './pages/home/home'
+
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar/>  
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        </Routes>          
+      </div>
+  )
+}
+
+export default App
